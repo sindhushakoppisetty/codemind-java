9db3025ef783e6f68e49677b7209dadf50e1adee
sindhushakoppisetty <96305586+sindhushakoppisetty@users.noreply.github.com>
2022-05-28 21:36:46 +0530

    import java.util.Scanner; class simple_calculator {    public static void main(String args[]) {       int A,B;       int Addition;       Scanner sc=new Scanner(System.in);       A=sc.nextInt();       B=sc.nextInt();       Addition=A+B;       System.out.println("" +Addition); } }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class simple_calculator
+{
+   public static void main(String args[])
+{
+      int A,B;
+      int Addition;
+      Scanner sc=new Scanner(System.in);
+      A=sc.nextInt();
+      B=sc.nextInt();
+      Addition=A+B;
+      System.out.println("" +Addition);
+}
+}
